775c450e6af79be1b370bee383a1007f2ca52864
Qutom <dlwlstjr0530@naver.com>
2022-06-24 11:32:51 +0900

    edit remove_wall cylinder to using open3d lib

---
+++ main.py
@@ -0,0 +1,26 @@
+import plane_extractor
+import BPD
+import numpy as np
+import remove_wall
+import open3d as o3d
+
+def display_outlier(points, ind):
+    inlier = points.select_by_index(ind)
+    outlier = points.select_by_index(ind, invert=True)
+    inlier.paint_uniform_color([0.7, 0.7, 0.7])
+    outlier.paint_uniform_color([1, 0, 0])
+    o3d.visualization.draw_geometries([inlier, outlier])
+
+pc = o3d.io.read_point_cloud("cropped_1.ply")
+#o3d.visualization.draw_geometries([pc])
+
+#plane, high_z, min_z = plane_extractor.extract_plane(pc)
+#boundary = BPD.cal_boundary(plane, save_filename="ceil_boundary.txt")
+boundary = np.loadtxt("ceil_boundary.txt")
+points = np.array(pc.points)
+high_z = float(np.max(points[:, 2]))
+min_z = float(np.min(points[:, 2]))
+props = remove_wall.remove_wall(points, boundary, method="cylinder", radius=0.07, min_z=min_z, high_z=high_z)
+
+o3d.visualization.draw_geometries([props])
+
--- remove_wall.py
+++ remove_wall.py
@@ -1,37 +1,38 @@
 import numpy as np
-
-"""
-@Params : points : list
-          boundary : list
-@Return  : float
-"""
-def distance_cylinder(point, boundary) :
-    return np.linalg.norm(point[:2] - boundary)
+import open3d as o3d
 
 """
 @Params : points : numpy array
           boundary : numpy array
           raidus : float
-@Return  : numpy array
+          min_z  : float
+          high_z : float
+@Return  : pointcloud data
 """
-def remove_cylinder(points, boundary, radius) :
-    for i in range(boundary.shape[0]) :
-        length = points.shape[0]
-        ind = []
-        print(i)
-        for j in range(length) :
-            if distance_cylinder(points[j], boundary[i]) <= radius :
-                ind.append(j)
-        points = np.delete(points, ind, 0)
+def remove_cylinder(points, boundary, radius, min_z, high_z) :
+    pc = o3d.geometry.PointCloud()
+    pc.points = o3d.utility.Vector3dVector(points)
 
-    return points
+    for i in range(boundary.shape[0]) :
+        z = np.arange(min_z, high_z, radius * 0.7)
+        pillar_points = np.tile(boundary[i], (z.shape[0], 1))
+
+        pillar_points = np.hstack((pillar_points, z.reshape(z.shape[0], 1)))
+        pillar_pc = o3d.geometry.PointCloud()
+        pillar_pc.points = o3d.utility.Vector3dVector(pillar_points)
+
+        dists = np.asarray(pc.compute_point_cloud_distance(pillar_pc))
+        ind = np.where(dists > radius)[0]
+        pc = pc.select_by_index(ind)
+        print(i)
+    return pc
 
 """
 @Params : points : numpy array
           boundary : numpy array
 @Return  : numpy array
 """
-def remove_wall(points, boundary, method="edge", radius=0.5, box_size=0.5) :
+def remove_wall(points, boundary, method="edge", radius=0.5, box_size=0.5, min_z=None, high_z=None) :
     if method == "edge" :
         #use edge algorithm
         pass
@@ -39,6 +40,6 @@ def remove_wall(points, boundary, method="edge", radius=0.5, box_size=0.5) :
         #use box algorithm
         pass
     if method == "cylinder" :
-        result = remove_cylinder(points, boundary, radius=radius)
+        result = remove_cylinder(points, boundary, radius=radius, min_z=min_z, high_z=high_z)
 
     return result
